7f282ac26e77791e1691681be0227aedc26d098c
@marichu_kt <145447256+marichu-kt@users.noreply.github.com>
2024-06-14 17:44:36 +0200

    Update Premio.cs

--- Slot-Machine-CSharp-UFV/Premio.cs
+++ Slot-Machine-CSharp-UFV/Premio.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +8,13 @@ namespace Slot_Machine_CSharp_UFV
 {
     internal abstract class Premio
     {
+        // PROPIEDADES COMUNES PREMIOS
         public string Nombre { get; set; }
         public int Simbolo1 { get; set; }
         public int Simbolo2 { get; set; }
         public int Simbolo3 { get; set; }
 
+        // CONSTRUCTOR PREMIOS
         public Premio(string nombre, int simbolo1, int simbolo2, int simbolo3)
         {
             Nombre = nombre;
@@ -21,6 +23,7 @@ namespace Slot_Machine_CSharp_UFV
             Simbolo3 = simbolo3;
         }
 
+        // METODO ABSTRACTO PARA MOSTRAR PREMIOS (se sobrescribe desde las clases hijas PremioSimple y PremioAleatorio)
         public abstract void MostrarConsejo();
     }
 }
